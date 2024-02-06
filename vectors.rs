37bc475c89e07d347cc00de6eab2500939b5d1ed
Sam Salgado <67569156+samsalgado@users.noreply.github.com>
2024-02-06 13:13:51 -0500

    Update vectors.rs

--- vectors.rs
+++ vectors.rs
@@ -1,28 +1,38 @@
-//Resizable arrays
+// Resizable arrays
 use std::mem;
+
 pub fn run() {
-let mut numbers: Vec<i32> = vec![1,2,22,3,4];
-numbers[2] = 20;
-//Add on to vector
-numbers.push(5);
-numbers.push(6);
-numbers.pop();
-println!("{:?}", numbers);
-println!("Single value: {}", numbers[0]);
-//GET VECTOR LENGTH
-println!("Vector length: {}", numbers.len());
-//stack allocated
-println!("Vector occupies {} bytes", mem::size_of_val(&numbers));
-//Get slice
-let slice: &[i32] = &numbers[0..2];
-println!("Slice: {:?}", slice);
-//Loop through vector values
-for x in numbers.iter() {
-    println!("Numbers: {}", x);
-}
-//Loop and mutate
-for x in numbers.iter_mut() {
-    *x *= 2;
-}
-println!("Numbers Vec: {:?}", numbers);
+    let mut numbers: Vec<i32> = vec![1, 2, 22, 3, 4];
+    numbers[2] = 20;
+
+    // Add on to vector
+    numbers.push(5);
+    numbers.push(6);
+    numbers.pop();
+
+    println!("Vector: {:?}", numbers);
+
+    // Single value
+    println!("First value: {}", numbers[0]);
+
+    // Vector length
+    println!("Vector length: {}", numbers.len());
+
+    // Stack allocated size
+    println!("Vector occupies {} bytes", mem::size_of_val(&numbers));
+
+    // Get a slice
+    let slice: &[i32] = &numbers[0..2];
+    println!("Slice: {:?}", slice);
+
+    // Loop through vector values
+    for x in &numbers {
+        println!("Number: {}", x);
+    }
+
+    // Loop and mutate
+    for x in &mut numbers {
+        *x *= 2;
+    }
+    println!("Doubled numbers: {:?}", numbers);
 }
